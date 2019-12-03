b6e5cc3db52fbd5daaec3935dcba485f8c23e97a
BadLuckBurt <cmjonasse@gmail.com>
2019-12-03 21:04:08 +0100

    Added crude blending where different terrains meet.

--- BLBTerrainSampler.cs
+++ BLBTerrainSampler.cs
@@ -76,107 +76,159 @@ public class BLBTerrainSampler : TerrainSampler
             int x = JobA.Col(index, hDim);
             int y = JobA.Row(index, hDim);
 
-            float rx = (float)x / div;
-            float ry = (float)y / div;
-            int ix = Mathf.FloorToInt(rx);
-            int iy = Mathf.FloorToInt(ry);
-            float sfracx = (float)x / (float)(hDim - 1);
-            float sfracy = (float)y / (float)(hDim - 1);
-            float fracx = (float)(x - ix * div) / div;
-            float fracy = (float)(y - iy * div) / div;
-            float scaledHeight = 0;
-
-            // Bicubic sample small height map for base terrain elevation
-            x1 = TerrainHelper.CubicInterpolator(shm[JobA.Idx(0, 3, sd)], shm[JobA.Idx(1, 3, sd)], shm[JobA.Idx(2, 3, sd)], shm[JobA.Idx(3, 3, sd)], sfracx);
-            x2 = TerrainHelper.CubicInterpolator(shm[JobA.Idx(0, 2, sd)], shm[JobA.Idx(1, 2, sd)], shm[JobA.Idx(2, 2, sd)], shm[JobA.Idx(3, 2, sd)], sfracx);
-            x3 = TerrainHelper.CubicInterpolator(shm[JobA.Idx(0, 1, sd)], shm[JobA.Idx(1, 1, sd)], shm[JobA.Idx(2, 1, sd)], shm[JobA.Idx(3, 1, sd)], sfracx);
-            x4 = TerrainHelper.CubicInterpolator(shm[JobA.Idx(0, 0, sd)], shm[JobA.Idx(1, 0, sd)], shm[JobA.Idx(2, 0, sd)], shm[JobA.Idx(3, 0, sd)], sfracx);
-            baseHeight = TerrainHelper.CubicInterpolator(x1, x2, x3, x4, sfracy);
-            if(worldHeight >= 70) {
-                scaledHeight += baseHeight * (baseHeightScale + (JobRand.Next(75) / 100));
-            } else {
-                scaledHeight += baseHeight * baseHeightScale;
-            }
-            
-            // Bicubic sample large height map for noise mask over terrain features
-            x1 = TerrainHelper.CubicInterpolator(lhm[JobA.Idx(ix, iy + 0, ld)], lhm[JobA.Idx(ix + 1, iy + 0, ld)], lhm[JobA.Idx(ix + 2, iy + 0, ld)], lhm[JobA.Idx(ix + 3, iy + 0, ld)], fracx);
-            x2 = TerrainHelper.CubicInterpolator(lhm[JobA.Idx(ix, iy + 1, ld)], lhm[JobA.Idx(ix + 1, iy + 1, ld)], lhm[JobA.Idx(ix + 2, iy + 1, ld)], lhm[JobA.Idx(ix + 3, iy + 1, ld)], fracx);
-            x3 = TerrainHelper.CubicInterpolator(lhm[JobA.Idx(ix, iy + 2, ld)], lhm[JobA.Idx(ix + 1, iy + 2, ld)], lhm[JobA.Idx(ix + 2, iy + 2, ld)], lhm[JobA.Idx(ix + 3, iy + 2, ld)], fracx);
-            x4 = TerrainHelper.CubicInterpolator(lhm[JobA.Idx(ix, iy + 3, ld)], lhm[JobA.Idx(ix + 1, iy + 3, ld)], lhm[JobA.Idx(ix + 2, iy + 3, ld)], lhm[JobA.Idx(ix + 3, iy + 3, ld)], fracx);
-            noiseHeight = TerrainHelper.CubicInterpolator(x1, x2, x3, x4, fracy);
-            scaledHeight += noiseHeight * noiseMapScale;
-
-            // Additional noise mask for small terrain features at ground level
-            int noisex = mapPixelX * (hDim - 1) + x;
-            int noisey = (MapsFile.MaxMapPixelY - mapPixelY) * (hDim - 1) + y;
-
-            /* Default values taken from the old sampler, overwritten by climate noise */
-            float lowFrequency = 0.3f;
-            float lowAmplitude = 0.5f;
-            float lowPersistence = 0.5f;
-            int lowOctaves = 1;
-            float lowScale = 1.0f;
-
-            float highFrequency = 0.9f;
-            float highAmplitude = 0.5f;
-            float highPersistence = 0.5f;
-            int highOctaves = 1;
-            float highScale = 1.0f;
-
-            float height = 0.0f;
-
             int noiseIndex = worldClimate;
-            bool north = (y < 32);
-            bool east = (x > 96);
-            bool south = (y > 96);
-            bool west = (x < 32);
+            bool north = (y > 80);
+            bool east = (x > 80);
+            bool south = (y <= 48);
+            bool west = (x <= 48);
             //When we are at the pixels borders, we check for different climates
             //In the appropriate direction and set the currentClimate accordingly
             //This way we can adjust the noise parameters so the terrain will blend
             //Regardless of climate noise
+            string direction = "";
+            int offset = 999;
             if (north && west) {
+                
                 if (noiseIndex != adjacentClimates[(int)MapPixelData.Adjacent.NorthWest]) {
                     noiseIndex = adjacentClimates[(int)MapPixelData.Adjacent.NorthWest];
+                    if(
+                        worldClimate == adjacentClimates[(int)MapPixelData.Adjacent.North] && 
+                        worldClimate == adjacentClimates[(int)MapPixelData.Adjacent.West]
+                    ) {
+                        if(x > 128 - y) {
+                            offset = x;
+                            direction = "w";
+                        } else {
+                            offset = 128 - y;
+                            direction = "n";
+                        }
+                    } else if(worldClimate == adjacentClimates[(int)MapPixelData.Adjacent.North]) {
+                        offset = x;
+                        direction = "w";
+                    } else if(worldClimate == adjacentClimates[(int)MapPixelData.Adjacent.West]) {
+                        offset = 128 - y;
+                        direction = "n";
+                    } else {
+                        offset = Mathf.Min(x, (128 - y));
+                        direction = "nw";
+                    }
                 } else if (noiseIndex != adjacentClimates[(int)MapPixelData.Adjacent.North]) {
                     noiseIndex = adjacentClimates[(int)MapPixelData.Adjacent.North];
+                    offset = (128 - y);
+                    direction = "n";
                 } else {
                     noiseIndex = adjacentClimates[(int)MapPixelData.Adjacent.West];
+                    direction = "w";
+                    offset = x;
                 }
             } else if (north && east) {
                 if (noiseIndex != adjacentClimates[(int)MapPixelData.Adjacent.NorthEast]) {
                     noiseIndex = adjacentClimates[(int)MapPixelData.Adjacent.NorthEast];
+                    if(
+                        worldClimate == adjacentClimates[(int)MapPixelData.Adjacent.North] && 
+                        worldClimate == adjacentClimates[(int)MapPixelData.Adjacent.East]
+                    ) {
+                        if(128 - x > 128 - y) {
+                            offset = 128 - x;
+                            direction = "e";
+                        } else {
+                            offset = 128 - y;
+                            direction = "n";
+                        }
+                    } else if (worldClimate == adjacentClimates[(int)MapPixelData.Adjacent.North]) {
+                        offset = 128 - x;
+                        direction = "e";
+                    } else if (worldClimate == adjacentClimates[(int)MapPixelData.Adjacent.East]) {
+                        offset = 128 - y;
+                        direction = "n";
+                    } else {
+                        offset = Mathf.Min((128 - x), (128 - y));
+                        direction = "ne";
+                    }
                 } else if (noiseIndex != adjacentClimates[(int)MapPixelData.Adjacent.North]) {
                     noiseIndex = adjacentClimates[(int)MapPixelData.Adjacent.North];
+                    offset = (128 - y);
+                    direction = "n";
                 } else {
                     noiseIndex = adjacentClimates[(int)MapPixelData.Adjacent.East];
+                    offset = (128 - x);
+                    direction = "e";
                 }
             } else if (south && west) {
                 if (noiseIndex != adjacentClimates[(int)MapPixelData.Adjacent.SouthWest]) {
                     noiseIndex = adjacentClimates[(int)MapPixelData.Adjacent.SouthWest];
+                    direction = "sw";
+                    if(
+                        worldClimate == adjacentClimates[(int)MapPixelData.Adjacent.South] && 
+                        worldClimate == adjacentClimates[(int)MapPixelData.Adjacent.West]
+                    ) {
+                        if(x > y) {
+                            offset = x;
+                        } else {
+                            offset = y;
+                        }
+                    } else if (worldClimate == adjacentClimates[(int)MapPixelData.Adjacent.South]) {
+                        offset = x;
+                    } else if (worldClimate == adjacentClimates[(int)MapPixelData.Adjacent.West]) {
+                        offset = y;
+                    } else {
+                        offset = Mathf.Min(x, y);
+                    }
                 } else if (noiseIndex != adjacentClimates[(int)MapPixelData.Adjacent.South]) {
                     noiseIndex = adjacentClimates[(int)MapPixelData.Adjacent.South];
+                    direction = "s";
+                    offset = y;
                 } else {
                     noiseIndex = adjacentClimates[(int)MapPixelData.Adjacent.West];
+                    direction = "w";
+                    offset = x;
                 }
             } else if (south && east) {
                 if (noiseIndex != adjacentClimates[(int)MapPixelData.Adjacent.SouthEast]) {
                     noiseIndex = adjacentClimates[(int)MapPixelData.Adjacent.SouthEast];
+                    direction = "se";
+                    if(
+                        worldClimate == adjacentClimates[(int)MapPixelData.Adjacent.South] && 
+                        worldClimate == adjacentClimates[(int)MapPixelData.Adjacent.East]
+                    ) {
+                        if(128 - x > y) {
+                            offset = 128 - x;
+                        } else {
+                            offset = y;
+                        }
+                    } else if(worldClimate == adjacentClimates[(int)MapPixelData.Adjacent.South]) {
+                        offset = 128 - x;
+                    } else if(worldClimate == adjacentClimates[(int)MapPixelData.Adjacent.East]) {
+                        offset = y;
+                    } else {
+                        offset = Mathf.Max((128 - x), y);
+                    }
                 } else if (noiseIndex != adjacentClimates[(int)MapPixelData.Adjacent.South]) {
                     noiseIndex = adjacentClimates[(int)MapPixelData.Adjacent.South];
+                    direction = "s";
+                    offset = y;
                 } else {
                     noiseIndex = adjacentClimates[(int)MapPixelData.Adjacent.East];
+                    direction = "e";
+                    offset = 128 - x;
                 }
             } else if (west) {
                 noiseIndex = adjacentClimates[(int)MapPixelData.Adjacent.West];
+                direction = "w";
+                offset = x;
             } else if (east) {
                 noiseIndex = adjacentClimates[(int)MapPixelData.Adjacent.East];
+                direction = "e";
+                offset = 128 - x;
             } else if (north) {
                 noiseIndex = adjacentClimates[(int)MapPixelData.Adjacent.North];
+                direction = "n";
+                offset = 128 - y;
             } else if (south) {
                 noiseIndex = adjacentClimates[(int)MapPixelData.Adjacent.South];
+                direction = "s";
+                offset = y;
             }
-
-            //The following section is work in progress, nicer transitions should be possible
 
             //Next we check what are our current climate is
             //The current blend order from weak to strong is:
@@ -204,6 +256,94 @@ public class BLBTerrainSampler : TerrainSampler
                     noiseIndex = worldClimate;
                 }
             }
+
+            float rx = (float)x / div;
+            float ry = (float)y / div;
+            int ix = Mathf.FloorToInt(rx);
+            int iy = Mathf.FloorToInt(ry);
+            float sfracx = (float)x / (float)(hDim - 1);
+            float sfracy = (float)y / (float)(hDim - 1);
+            float fracx = (float)(x - ix * div) / div;
+            float fracy = (float)(y - iy * div) / div;
+            float scaledHeight = 0;
+
+            // Bicubic sample small height map for base terrain elevation
+            x1 = TerrainHelper.CubicInterpolator(shm[JobA.Idx(0, 3, sd)], shm[JobA.Idx(1, 3, sd)], shm[JobA.Idx(2, 3, sd)], shm[JobA.Idx(3, 3, sd)], sfracx);
+            x2 = TerrainHelper.CubicInterpolator(shm[JobA.Idx(0, 2, sd)], shm[JobA.Idx(1, 2, sd)], shm[JobA.Idx(2, 2, sd)], shm[JobA.Idx(3, 2, sd)], sfracx);
+            x3 = TerrainHelper.CubicInterpolator(shm[JobA.Idx(0, 1, sd)], shm[JobA.Idx(1, 1, sd)], shm[JobA.Idx(2, 1, sd)], shm[JobA.Idx(3, 1, sd)], sfracx);
+            x4 = TerrainHelper.CubicInterpolator(shm[JobA.Idx(0, 0, sd)], shm[JobA.Idx(1, 0, sd)], shm[JobA.Idx(2, 0, sd)], shm[JobA.Idx(3, 0, sd)], sfracx);
+            baseHeight = TerrainHelper.CubicInterpolator(x1, x2, x3, x4, sfracy);
+            scaledHeight += baseHeight * baseHeightScale;
+            
+            // Bicubic sample large height map for noise mask over terrain features
+            x1 = TerrainHelper.CubicInterpolator(lhm[JobA.Idx(ix, iy + 0, ld)], lhm[JobA.Idx(ix + 1, iy + 0, ld)], lhm[JobA.Idx(ix + 2, iy + 0, ld)], lhm[JobA.Idx(ix + 3, iy + 0, ld)], fracx);
+            x2 = TerrainHelper.CubicInterpolator(lhm[JobA.Idx(ix, iy + 1, ld)], lhm[JobA.Idx(ix + 1, iy + 1, ld)], lhm[JobA.Idx(ix + 2, iy + 1, ld)], lhm[JobA.Idx(ix + 3, iy + 1, ld)], fracx);
+            x3 = TerrainHelper.CubicInterpolator(lhm[JobA.Idx(ix, iy + 2, ld)], lhm[JobA.Idx(ix + 1, iy + 2, ld)], lhm[JobA.Idx(ix + 2, iy + 2, ld)], lhm[JobA.Idx(ix + 3, iy + 2, ld)], fracx);
+            x4 = TerrainHelper.CubicInterpolator(lhm[JobA.Idx(ix, iy + 3, ld)], lhm[JobA.Idx(ix + 1, iy + 3, ld)], lhm[JobA.Idx(ix + 2, iy + 3, ld)], lhm[JobA.Idx(ix + 3, iy + 3, ld)], fracx);
+            noiseHeight = TerrainHelper.CubicInterpolator(x1, x2, x3, x4, fracy);
+            scaledHeight += noiseHeight * noiseMapScale;
+
+            // Additional noise mask for small terrain features at ground level
+            int noisex = mapPixelX * (hDim - 1) + x;
+            int noisey = (MapsFile.MaxMapPixelY - mapPixelY) * (hDim - 1) + y;
+
+            float noise = Mathf.PerlinNoise(noisex * 0.032f, noisey * 0.032f);
+            if(worldClimate != noiseIndex && direction != "") {
+                float heightSum = 0.0f;
+                int multiplier = offset;
+                int multiplier2 = 48 - multiplier;
+                if(worldHeight <= 2) {
+                    scaledHeight = scaledOceanElevation;
+                } else {
+                    heightSum = GetClimateHeight(worldClimate, scaledHeight, noise) * multiplier;
+                    heightSum += GetClimateHeight(noiseIndex, scaledHeight, noise) * multiplier2;
+                    multiplier += multiplier2;
+                    scaledHeight = heightSum / multiplier;
+                }
+            } else {
+                switch(noiseIndex) {
+                    case 0: //Ocean
+
+                        break;
+                    case 1: //Swamp
+                        if(scaledHeight >= scaledBeachElevation) {
+
+                            scaledHeight = GetClimateHeight(noiseIndex, scaledBeachElevation, noise);
+                            //scaledHeight = scaledBeachElevation - (4f * (noise + 0.125f));
+                        }
+                        break;
+                    case 2: //Desert
+                        if(scaledHeight >= scaledBeachElevation) {
+                            scaledHeight = GetClimateHeight(noiseIndex, scaledBeachElevation, noise);
+                            //scaledHeight = scaledBeachElevation + (2f * (noise));
+                        }
+                        break;
+                    case 3: //Mountain
+                        scaledHeight = GetClimateHeight(noiseIndex, scaledHeight, noise);
+                        //scaledHeight += (scaledHeight / 4) * noise;
+                        break;
+                    case 4://Temperate
+
+                        break;
+                }
+            }
+            /* Default values taken from the old sampler, overwritten by climate noise */
+            float lowFrequency = 0.3f;
+            float lowAmplitude = 0.5f;
+            float lowPersistence = 0.5f;
+            int lowOctaves = 1;
+            float lowScale = 1.0f;
+
+            float highFrequency = 0.9f;
+            float highAmplitude = 0.5f;
+            float highPersistence = 0.5f;
+            int highOctaves = 1;
+            float highScale = 1.0f;
+
+            float height = 0.0f;
+
+            //The following section is work in progress, nicer transitions should be possible
+
             //Retrieve the appropriate noise settings
             lowFrequency = modSettings[noiseIndex,0];
             lowAmplitude = modSettings[noiseIndex,1];
@@ -294,6 +434,22 @@ public class BLBTerrainSampler : TerrainSampler
     {
         for(int i = 0; i < adjacentClimates.Length; i++)
             adjacentClimates[i] = GetClimateNoiseIndex(adjacentClimates[i]);
+    }
+
+    private static float GetClimateHeight(int noiseIndex, float height, float noise) {
+        switch(noiseIndex) {
+            case 0: //Ocean
+                return scaledOceanElevation;
+            case 1: //Swamp
+                return scaledBeachElevation - (4f * (noise + 0.125f));
+            case 2: //Desert
+                return scaledBeachElevation + (2f * (noise));
+            case 3: //Mountain
+                return height + (((height * 1.15f) / 4) * noise);
+            case 4: //Temperate
+                return height;
+        }
+        return height;
     }
 
     private static byte GetClimateNoiseIndex(byte worldClimate)
